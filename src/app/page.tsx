3a6b9c86abeb6c9b78dd08bbf8581c7083789643
postpostscript <danielrw7@gmail.com>
2025-08-09 08:49:42 -0400

    Fix link list responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,29 @@ import { EmailLink } from "@/components/Email";
 
 export default function Home() {
   return (
-    <div className="font-mono min-h-screen p-8 sm:p-20 flex-col gap-20 flex max-w-[40rem] mx-auto">
-      <main className="flex flex-col gap-6 row-start-2 items-center sm:items-start">
-        <h1 className="sm:w-full flex sm:justify-between text-[2.75rem] sm:text-7xl">
+    <div className="mx-auto flex min-h-screen max-w-[40rem] flex-col gap-20 p-8 font-mono sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-6 sm:items-start">
+        <h1 className="flex text-[2.75rem] sm:w-full sm:justify-between sm:text-7xl">
           <span>Danny</span>&nbsp;<span>Wilson</span>
         </h1>
-        <h2 className="sm:w-full flex sm:justify-between text-xl sm:text-[2rem]">
+        <h2 className="flex text-xl sm:w-full sm:justify-between sm:text-[2rem]">
           <span>Senior&nbsp;</span>
           <em>Full&nbsp;</em>
           <em>Stack&nbsp;</em>Developer
         </h2>
 
-        <div className="flex justify-around w-full text-xl leading-5">
-          <ul className="flex gap-4">
-            <li className="pe-4 border-e-2">
+        <div className="flex w-full justify-around text-xl sm:leading-5">
+          <ul className="flex flex-col gap-4 sm:flex-row">
+            <li className="border-foreground block text-center max-sm:border-b-2 max-sm:pb-4 sm:border-r-2 sm:pr-4">
               <a href="https://github.com/postpostscript">GitHub</a>
             </li>
-            <li className="pe-4 border-e-2">
+            <li className="border-foreground block text-center max-sm:border-b-2 max-sm:pb-4 sm:border-r-2 sm:pr-4">
               <a href="https://www.linkedin.com/in/drwxr-xr-x/">LinkedIn</a>
             </li>
-            <li className="pe-4 border-e-2">
+            <li className="border-foreground block text-center max-sm:border-b-2 max-sm:pb-4 sm:border-r-2 sm:pr-4">
               <a href="https://postpostscript-blog.web.val.run/">Val Town</a>
             </li>
-            <li>
+            <li className="text-center">
               <EmailLink />
             </li>
           </ul>
